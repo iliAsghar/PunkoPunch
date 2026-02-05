6e0f0b75b19769081082cd2c88d95f9af3f38b63
ili <asghar.ali.z.a.r.e@gmail.com>
2026-02-05 13:04:12 +0330

    Thank you message (easteregg)

--- frontend/scenes/MenuScene.js
+++ frontend/scenes/MenuScene.js
@@ -7,6 +7,11 @@ class MenuScene extends Phaser.Scene {
         const width = this.cameras.main.width;
         const height = this.cameras.main.height;
 
+        this.add.text(width / 2, height / 2 - 300, "thanks for testing this. I'm talking to you sith! ;D", {
+            font: 'bold 32px Arial',
+            fill: '#000000'
+        }).setOrigin(0.5);
+
         // Game Title
         this.add.text(width / 2, height / 2 - 150, 'PunkoPunch', {
             font: 'bold 64px Arial',
